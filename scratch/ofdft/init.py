f462f6c4b1612a86f78801975b9ec0f35ac53fff
Amonsuzuki <amonsuzuki@keio.jp>
2026-02-16 19:31:56 +0900

    ref_etb

--- scratch/ofdft/init.py
+++ scratch/ofdft/init.py
@@ -1,3 +1,6 @@
+import pyscf
+import pyscf.df
+
 def get_init_coeff(init_method, mol, auxmol, *args, use_dm=False, **kwargs):
     methods = {
             'initguess_minao': initguess_minao,
@@ -11,3 +14,8 @@ def get_init_coeff(init_method, mol, auxmol, *args, use_dm=False, **kwargs):
             }
     method = methods[init_nethod]
     return method(mol, auxmol, *args, use_dm=use_dm, **kwargs)
+
+def ref_etb(mol, beta):
+    reference_mol = pyscf.M(atom='H 0 0 0; C 0 -0.5 0.5; N 0 0.5 1; O 0 0.5 -0.5; F 0.5 0 0', basis=mol.basis, spin=1)
+    basis = pyscf.df.aug_etb(reference_mol, beta)
+    return basis
